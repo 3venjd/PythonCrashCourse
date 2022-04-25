1031ee1661472f6048954780f573bc5575d2fad1
3velio <lui.eve.jim.dur@gmail.com>
2022-04-25 15:59:20 -0500

    last update 4-25-2022

---
+++ Chapter_4/even_numbers.py
@@ -0,0 +1,4 @@
+#						start,end,increment
+even_numbers = list(range(2, 11, 2))
+print(even_numbers)
+
---
+++ Chapter_4/exercise4_1.py
@@ -0,0 +1,12 @@
+#pizzas = ['sweet pork', 'margarita', 'colombian']
+
+#for pizza in pizzas:
+#	print(f"i like {pizza} pizza")
+
+#print("\nI really love pizza")
+
+animals = ['wolf', 'dog', 'fox']
+for animal in animals:
+	print(f"a {animal} would make a great pet")
+
+print(f"all of these animals has a pretty fur")
