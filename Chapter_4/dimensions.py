211471a515bd85318fcb53ddc93aef566af4d43e
3venjd <lui.eve.jim.dur@gmail.com>
2022-09-07 16:33:50 -0500

    End chapter 4

--- Chapter_4/dimensions.py
+++ Chapter_4/dimensions.py
@@ -1,4 +1,17 @@
 dimensions = (200,50)
-print(dimensions[0])
-print(dimensions[1])
+#print(dimensions[0])
+#print(dimensions[1])
 
+#Error because a tuple is inmutable
+#dimensions[0] = 250
+
+print('original dimensions')
+for dim in dimensions:
+	print(dim)
+
+#to modify a tuple we need to change all data
+dimensions = (400,100)
+
+print('\nModified dimensions')
+for dim in dimensions:
+	print(dim)
---
+++ Chapter_4/excercise4_13.py
@@ -0,0 +1,10 @@
+basic_foods = ('meat', 'juice', 'milk', 'carrot', 'salt')
+
+for bfood in basic_foods:
+	print(bfood)
+
+##basic_foods[2] = 'yogurt'
+
+Modified_Basic_Foods = ('meat', 'juice', 'banana', 'carrot', 'suggar')
+for bfood in Modified_Basic_Foods:
+	print(bfood)
