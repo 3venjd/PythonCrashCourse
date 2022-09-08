c8a9c7248e5c66855afebb6b932bb331b18616d0
3venjd <lui.eve.jim.dur@gmail.com>
2022-09-07 22:58:45 -0500

    chapter 5 page 143

---
+++ Chapter_5/excersice_5_1.py
@@ -0,0 +1,8 @@
+car = 'subaru'
+
+if car == 'subaru':
+	print("i predict True")
+	print("car is a subaru")
+else:
+	print("i predict False")
+	print("car is not a subaru")
--- Chapter_5/toppings.py
+++ Chapter_5/toppings.py
@@ -2,3 +2,9 @@ requested_topping = 'mushrooms'
 
 if requested_topping != 'anchovies':
 	print('Hold the anchovies')
+
+
+list_requested_topping = ['mushrooms', 'onions', 'pineapple']
+
+print('mushrooms' in list_requested_topping)
+print('peperonni' in list_requested_topping)
