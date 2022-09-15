a8c92d408db3503c691dd82c7ebec520e3afe850
3venjd <lui.eve.jim.dur@gmail.com>
2022-09-14 19:32:58 -0500

    Chapter 6 171 try it

--- Chapter_6/alien.py
+++ Chapter_6/alien.py
@@ -23,21 +23,27 @@
 #alien_0['color'] = 'yellow'
 #print(f"the alien is now {alien_0['color']}")
 
-alien_0 = {'x_position' : 0, 'y_position' : 25, 'speed' : 'medium'}
-print(f"Original position: {alien_0['x_position']}")
+#alien_0 = {'x_position' : 0, 'y_position' : 25, 'speed' : 'medium'}
+#print(f"Original position: {alien_0['x_position']}")
 
 # move the alien to the right
 # determine how far to move the alien based on its current speed
 
-if alien_0['speed'] == 'slow':
-	x_increment = 1
-elif alien_0['speed'] == 'medium':
-	x_increment = 2
-else:
+#if alien_0['speed'] == 'slow':
+#	x_increment = 1
+#elif alien_0['speed'] == 'medium':
+#	x_increment = 2
+#else:
 	#this must be a fast alien
-	x_increment = 3
+#	x_increment = 3
 
 #the new position is the old position plus the incremenet
-alien_0['x_position'] = alien_0['x_position'] + x_increment
+#alien_0['x_position'] = alien_0['x_position'] + x_increment
 
-print(f"new position : {alien_0['x_position']}")
+#print(f"new position : {alien_0['x_position']}")
+
+alien_0 = {'color' : 'green', 'points' : 5}
+print(alien_0)
+
+del alien_0['points']
+print(alien_0)
