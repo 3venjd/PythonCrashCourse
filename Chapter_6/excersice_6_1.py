e1d4202a961bdc27fd8bbd35981c80afba1195ea
3venjd <lui.eve.jim.dur@gmail.com>
2022-09-16 19:27:35 -0500

    c6 171

---
+++ Chapter_6/excersice_6_1.py
@@ -0,0 +1,4 @@
+person = {'first_Name' : 'Ringa', 'Last_Name' : 'Tech', 'Age' : 30, 'City' : 'Frankfurt'}
+
+for	x in person:
+	print(person[x])
---
+++ Chapter_6/excersice_6_2.py
@@ -0,0 +1,4 @@
+favoriteNumber = {'wendy' : 2, 'cris' : 4, 'eve' : 8, 'kat' : 12}
+
+for x in favoriteNumber:
+	print(f"the favorite number of {x} is {favoriteNumber[x]}")
