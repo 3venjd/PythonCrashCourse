fec1c4dab3b96abdad310ee49ab61c62434f7e0c
3venjd <lui.eve.jim.dur@gmail.com>
2022-09-16 21:59:49 -0500

    c6 178

---
+++ Chapter_6/excersice_6_3.py
@@ -0,0 +1,9 @@
+programming_words = {
+	'int' : 'type of data that save int values',
+	 'list' : 'array that contains data',
+	 'tuple' : 'unmutable list',
+	 'Dictionary' : 'list that contains data with key-value'
+	  }
+
+for word in programming_words:
+	print(f"{word} is a {programming_words[word]}\n")
--- Chapter_6/favorite_languages.py
+++ Chapter_6/favorite_languages.py
@@ -5,6 +5,29 @@ favorite_languages = {
 	'pil' : 'python'
 }
 
-language = favorite_languages['sarah'].title()
+#language = favorite_languages['sarah'].title()
 
-print(f"Sarah's favorite language is {language}")
+#print(f"Sarah's favorite language is {language}")
+
+#for name, language in favorite_languages.items():
+#	print(f"{name.title()}'s favorite languages is {language.title()}.")
+
+#print only the keys
+#for x in favorite_languages.keys():
+#	print(x.title())
+
+#	friends = ['pil', 'sarah']
+#	if name in friends:
+#		language = favorite_languages[name].title()
+#		print(f"\t{name.title()}, i see you love {language}")
+
+#if 'erin' not in favorite_languages.keys():
+	#print("Erin, please take our poll")
+
+#for name in sorted(favorite_languages.keys()):
+#	print(f"{name.title()}, thank you for taking the poll")
+
+print("the following languages have been mentionated: ")
+
+for languages in favorite_languages.values():
+	print(languages.title())
