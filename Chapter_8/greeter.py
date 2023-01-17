5f3d566c527f72f756865a338b1f066aa99314f2
3venjd <lui.eve.jim.dur@gmail.com>
2023-01-16 23:02:31 -0500

    Cap 8 Pag 228 Modifying a list in function

--- Chapter_8/greeter.py
+++ Chapter_8/greeter.py
@@ -8,24 +8,33 @@
 
 #greet_user('Jesse')
 
-def get_formatted_name(first_name, last_name):
-    """Return a full name, neatly formatted"""
-    full_name = f"{first_name} {last_name}"
-    return full_name.title()
+#def get_formatted_name(first_name, last_name):
+#    """Return a full name, neatly formatted"""
+#    full_name = f"{first_name} {last_name}"
+#    return full_name.title()
 
 # this is an infinite loop!
-while True:
-    print("\nPlease tell me your name")
-    print("(enter 'q' at any tume to quit)")
+#while True:
+#    print("\nPlease tell me your name")
+#    print("(enter 'q' at any tume to quit)")
 
-    f_name = input("First name: ")
-    if f_name == 'q':
-        break
+#    f_name = input("First name: ")
+#    if f_name == 'q':
+#        break
 
-    l_name = input("Last name: ")
-    if l_name == 'q':
-        break
+#    l_name = input("Last name: ")
+#    if l_name == 'q':
+#        break
 
-    formatted_name = get_formatted_name(f_name,l_name)
-    print(f"\nHello, {formatted_name}")
+#    formatted_name = get_formatted_name(f_name,l_name)
+#    print(f"\nHello, {formatted_name}")
 
+def greet_users(names):
+    """Print a simpple greeting to each user in the list"""
+    for name in names:
+        msg = f"Hello, {name.title()}!"
+        print(msg)
+    
+
+userNames = ['hannah', 'ty', 'margot']
+greet_users(userNames)
