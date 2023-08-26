0a0e4d41c48ab891df67ead3bcc9ac496b9155a9
luis jimenez <eve_ghetto@hotmail.com>
2023-08-26 08:54:35 -0500

    End Test Capter 11

---
+++ Chapter_11/Excersice_11_3/test_employes_function.py
@@ -0,0 +1,23 @@
+import unittest
+from employes import employe
+
+class TestEmployeSalaryRise(unittest.TestCase):
+
+    def setUp(self):
+        """Create a employee and test if the survey works"""
+        firstName = "Rafael"
+        lastName = "Jimenez"
+        salary = 6000
+        self.employe = employe(firstName,lastName,salary)
+
+    #when we test integer, always cast into string
+    def test_give_default_rise(self):
+        self.employe.give_raise()
+        self.assertIn("11000",str(self.employe.dataEmploye["ActualSalary"]))
+
+    def test_give_custom_rise(self):
+        self.employe.give_raise(2000)
+        self.assertIn("8000",str(self.employe.dataEmploye["ActualSalary"]))
+
+if __name__ == '__main__':
+    unittest.main()
