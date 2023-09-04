ad78518cadaf54c80855199b001ae41914ef4a27
luis jimenez <eve_ghetto@hotmail.com>
2023-09-04 13:03:40 -0500

    bullets added

--- ProjectAlienInvasion/alien_invasion.py
+++ ProjectAlienInvasion/alien_invasion.py
@@ -2,6 +2,8 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
+
 
 class AlienIvasion:
     """Overall class to manage game assets and behavior"""
@@ -19,6 +21,7 @@ class AlienIvasion:
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
     def run_game(self):
         """Start the main loop for the game"""
@@ -26,6 +29,8 @@ class AlienIvasion:
             #use a helpers method to allow manage events separately
             self._check_events()
             self.ship.update()
+            self._update_bullets()
+
             self._update_screen()
 
 
@@ -49,7 +54,10 @@ class AlienIvasion:
             self.ship.moving_left = True
         elif event.key == pygame.K_q:
             sys.exit()
-    
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
+
+
     def _check_keyup_events(self,event):
         """Respond to key releases"""
         if event.key == pygame.K_RIGHT:
@@ -58,16 +66,35 @@ class AlienIvasion:
             #move the ship to the left
             self.ship.moving_left = False
 
+    def _fire_bullet(self):
+        """Create a new bullet and add it to the bullets group"""
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
 
 
     def _update_screen(self):
         # Redraw the screen during  each pass through the loop
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
 
         #Make the most recently drawn screen visible
         pygame.display.flip()
+    
+    def _update_bullets(self):
+        """Update position of bullets and get rid of old bullets"""
+        #Update bullet positions
+        self.bullets.update()
 
+        #Get rid of bullets that have disappeared
+        for bullet in self.bullets.copy(): #1
+            if bullet.rect.bottom <= 0:#2
+                self.bullets.remove(bullet) #3
+        
+        
 if __name__ == '__main__':
     #Make a game instance, and run the game 
     ai = AlienIvasion()
