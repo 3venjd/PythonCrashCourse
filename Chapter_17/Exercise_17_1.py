46ebaae064e67196aa385b9dd8529432f2c807d9
3venjd <eve_ghetto@hotmail.com>
2023-10-12 10:57:09 -0500

    End Chapter 17

--- Chapter_17/Exercise_17_1.py
+++ Chapter_17/Exercise_17_1.py
@@ -2,7 +2,8 @@ import requests
 
 # Make an API call and store the response
 
-url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+language = 'c#'
+url = f'https://api.github.com/search/repositories?q=language:{language}&sort=stars'
 
 headers = {'Accept': 'application/vnd.github.v3+json'}
 r = requests.get(url, headers=headers)
